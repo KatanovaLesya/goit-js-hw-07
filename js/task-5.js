34ab459368006ab5f36dfd342740a52cbf71201e
KatanovaLesya <katanovalesya@gmail.com>
2024-09-19 09:53:57 +0300

    task5

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,17 @@
+// Функція для генерування випадкового кольору у форматі HEX
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  const changeColorButton = document.querySelector('.change-color');
+  const colorSpan = document.querySelector('.color');
+  const body = document.body;
+
+  // Обробник події кліку по кнопці
+  changeColorButton.addEventListener('click', () => {
+    const newColor = getRandomHexColor();
+    body.style.backgroundColor = newColor; // Змінюємо колір фону body
+    colorSpan.textContent = newColor; // Відображаємо значення кольору в span
+  });
+});
