2ff4ab922cde03a744bca321e04cb7d26e7c0fa5
KatanovaLesya <katanovalesya@gmail.com>
2024-09-19 09:47:33 +0300

    task4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,30 @@
+// Отримуємо посилання на форму
+const loginForm = document.querySelector('.login-form');
+
+// Додаємо слухач події submit
+loginForm.addEventListener('submit', (event) => {
+    event.preventDefault(); // Зупиняємо перезавантаження сторінки
+
+    // Отримуємо значення полів форми
+    const email = loginForm.elements.email.value.trim();
+    const password = loginForm.elements.password.value.trim();
+
+    // Перевірка, чи всі поля заповнені
+    if (email === '' || password === '') {
+        alert('All form fields must be filled in');
+        return; // Зупиняємо подальше виконання, якщо є порожні поля
+    }
+
+    // Створюємо об'єкт з даними форми
+    const formData = {
+        email,
+        password,
+    };
+
+    // Виводимо об'єкт з даними в консоль
+    console.log(formData);
+
+    // Очищуємо форму після сабміту
+    loginForm.reset();
+});
 
